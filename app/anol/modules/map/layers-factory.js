681e82b7470d15a1bd2487ee9484c7fa87511811
Kai Culemann <culemann@omniscale.de>
2014-10-17 16:03:15 +0200

    Used LayersFactory to create TMS layer

---
+++ app/anol/modules/map/layers-factory.js
@@ -0,0 +1,49 @@
+angular.module('anol.map')
+.provider('LayersFactory', [function() {
+    var self = this;
+
+    this.newTMS = function(options) {
+        var tileGrid = false;
+
+        var tileURL = function(tileCoord, pixelRatio, projection) {
+            var url = '';
+            if (tileCoord[1] > 0 && tileCoord[2] > 0) {
+                url += options.baseURL + '/';
+                url += options.layer + '/';
+                url += tileCoord[0].toString() + '/';
+                url += tileCoord[1].toString() + '/';
+                url += tileCoord[2].toString();
+                url += '.' + options.format;
+            }
+            return url;
+        };
+
+        if(options.extent && options.resolutions) {
+            tileGrid = new ol.tilegrid.TileGrid({
+                origin: [options.extent[0], options.extent[1]],
+                resolutions: options.resolutions
+            });
+        }
+
+        var sourceOptions = {
+            tileUrlFunction: tileURL
+        }
+        if(tileGrid) {
+            sourceOptions.tileGrid = tileGrid;
+        }
+        if(options.projection !== undefined) {
+            sourceOptions.projection = options.projection;
+        }
+        if(options.atrtibution !== undefined) {
+            sourceOptions.attributions = options.attributions;
+        }
+
+        return new ol.layer.Tile({
+            source: new ol.source.TileImage(sourceOptions)
+        });
+    };
+
+    this.$get = [function() {
+        return self;
+    }];
+}]);
--- app/config.js
+++ app/config.js
@@ -1,13 +1,53 @@
 angular.module('orkaApp', ['anol.map', 'anol.scaleline', 'anol.mouseposition'])
 
-.config(function (LayersServiceProvider, ViewServiceProvider, ControlsServiceProvider) {
+.config(function (LayersServiceProvider, ViewServiceProvider, ControlsServiceProvider, LayersFactoryProvider) {
+    var projection = new ol.proj.Projection({
+        code: 'EPSG:25388',
+        units: 'm'
+    });
+
+    var extent = [200000, 5880000, 480000, 6075000];
+    var resolutions = [
+        529.166666667,
+        352.777777778,
+        264.583333333,
+        176.388888889,
+        88.1944444444,
+        52.9166666667,
+        35.2777777778,
+        28.2222222222,
+        22.9305555556,
+        17.6388888889,
+        12.3472222222,
+        8.8194444444,
+        7.0555555556,
+        5.2916666667,
+        3.5277777778,
+        2.6458333333,
+        1.7638888889,
+        0.8819444444,
+        0.3527777778,
+        0.1763888889
+    ];
+
     ViewServiceProvider.setView(new ol.View({
-        center: [0, 0],
-        zoom: 2
+        projection: projection,
+        center: [313282, 6003693],
+        resolutions: resolutions,
+        zoom: 9
     }));
 
+    var tms = LayersFactoryProvider.newTMS({
+        resolutions: resolutions,
+        format: 'png',
+        extent: extent,
+        baseURL: 'http://geo.sv.rostock.de/geodienste/stadtplan/tms/1.0.0/',
+        layer: 'stadtplan_EPSG25833',
+        projection: projection
+    });
+
     LayersServiceProvider.setLayers([
-        new ol.layer.Tile({source: new ol.source.OSM()})
+        tms
     ]);
 
     ControlsServiceProvider.setControls(
