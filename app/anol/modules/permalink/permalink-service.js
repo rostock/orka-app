19ed5611b2a9dcc6f6299542e08c9e53b7c54355
Kai Culemann <culemann@omniscale.de>
2014-10-23 09:05:23 +0200

    Refactored PermalinkService to increase readability

--- app/anol/modules/permalink/permalink-service.js
+++ app/anol/modules/permalink/permalink-service.js
@@ -20,64 +20,60 @@ angular.module('anol.permalink', [])
         _urlCrs = crs;
     };
 
-    var Permalink = function($rootScope, $location, MapService, LayersService, urlCrs) {
-        var self = this;
-        self.urlCrs = urlCrs;
-        self.$rootScope = $rootScope;
-        self.$location = $location;
-        self.LayersService = LayersService;
-        self.zoom = undefined;
-        self.lon = undefined;
-        self.lat = undefined;
-        self.layersShortcuts = undefined;
-        self.map = MapService.getMap();
-        self.view = self.map.getView();
+    this.$get = ['$rootScope', '$location', 'MapService', 'LayersService', function($rootScope, $location, MapService, LayersService) {
+        var Permalink = function(urlCrs) {
+            var self = this;
+            self.urlCrs = urlCrs;
+            self.zoom = undefined;
+            self.lon = undefined;
+            self.lat = undefined;
+            self.layersShortcuts = undefined;
+            self.map = MapService.getMap();
+            self.view = self.map.getView();
 
-        self.map.on('moveend', self.moveendHandler, self);
+            self.map.on('moveend', self.moveendHandler, self);
 
-        self.$rootScope.$watchCollection(
-            function() {
-                return self.LayersService.visibleLayerShortcuts;
-            }, function(newVal, oldVal) {
-                if(newVal !== undefined) {
-                    self.layersShortcuts = newVal.join('');
-                    self.generatePermalink();
+            $rootScope.$watchCollection(
+                function() {
+                    return LayersService.visibleLayerShortcuts;
+                }, function(newVal, oldVal) {
+                    if(newVal !== undefined) {
+                        self.layersShortcuts = newVal.join('');
+                        self.generatePermalink();
+                    }
+                }
+            );
+
+            var mapParams = extractMapParams($location.path());
+            if(mapParams !== false) {
+                var center = ol.proj.transform(mapParams.center, mapParams.crs, self.view.getProjection());
+                self.view.setCenter(center);
+                self.view.setZoom(mapParams.zoom);
+                if(mapParams.shortcuts !== undefined) {
+                    LayersService.setVisibleByShortcuts(mapParams.shortcuts);
                 }
             }
-        );
+        };
+        Permalink.prototype.moveendHandler = function(evt) {
+            var self = this;
+            var center = ol.proj.transform(self.view.getCenter(), self.view.getProjection(), self.urlCrs);
+            self.lon = center[0];
+            self.lat = center[1];
 
-        var mapParams = extractMapParams(self.$location.path());
-        if(mapParams !== false) {
-            var center = ol.proj.transform(mapParams.center, mapParams.crs, self.view.getProjection());
-            self.view.setCenter(center);
-            self.view.setZoom(mapParams.zoom);
-            if(mapParams.shortcuts !== undefined) {
-                self.LayersService.setVisibleByShortcuts(mapParams.shortcuts);
+            self.zoom = self.view.getZoom();
+            $rootScope.$apply(function() {
+                self.generatePermalink();
+            });
+        };
+        Permalink.prototype.generatePermalink = function(evt) {
+            var self = this;
+            if(self.zoom === undefined || self.lon === undefined || self.lat === undefined) {
+                return;
             }
-        }
-    };
-    Permalink.prototype.moveendHandler = function(evt) {
-        var self = this;
-        var center = ol.proj.transform(self.view.getCenter(), self.view.getProjection(), self.urlCrs);
-        self.lon = center[0];
-        self.lat = center[1];
-
-        self.zoom = self.view.getZoom();
-        self.$rootScope.$apply(function() {
-            self.generatePermalink();
-        });
-    };
-    Permalink.prototype.generatePermalink = function(evt) {
-        var self = this;
-        if(self.zoom === undefined || self.lon === undefined || self.lat === undefined) {
-            return;
-        }
-        var urlAddon = 'map=' + [self.zoom, self.lon, self.lat, self.urlCrs, self.layersShortcuts].join('/');
-        self.$location.path(urlAddon);
-        self.permalink = self.$location.absUrl();
-    };
-
-    this.$get = ['$rootScope', '$location', 'MapService', 'LayersService', function($rootScope, $location, MapService, LayersService) {
-        return new Permalink($rootScope, $location, MapService, LayersService, _urlCrs);
+            var urlAddon = 'map=' + [self.zoom, self.lon, self.lat, self.urlCrs, self.layersShortcuts].join('/');
+            $location.path(urlAddon);
+            self.permalink = $location.absUrl();
+        };
+        return new Permalink(_urlCrs);
     }];
 }]);
