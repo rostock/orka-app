e8d541f6104f000825406dc63638cc03e325ef4b
Kai Culemann <culemann@omniscale.de>
2014-10-27 13:11:20 +0100

    Implemented start print action

--- app/anol/modules/print/print-directive.js
+++ app/anol/modules/print/print-directive.js
@@ -1,6 +1,6 @@
 angular.module('anol.print', [])
 
-.directive('anolPrint', ['PrintService', function(PrintService) {
+.directive('anolPrint', ['PrintService', 'LayertreeService', 'LayersService', function(PrintService, LayertreeService, LayersService) {
     return {
         restrict: 'A',
         transclude: true,
@@ -12,7 +12,26 @@ angular.module('anol.print', [])
                 return scope.pageSize !== undefined && scope.pageSize[0] !== undefined && scope.pageSize[1] !== undefined && scope.scale !== undefined;
             };
             scope.startPrint = function() {
-                console.warn('Implement me');
+                scope.downloadUrl = false;
+                var layerName;
+
+                // TODO replace with LayersService.backgroundLayer if present in LayersService
+                angular.forEach(LayersService.backgroundLayers, function(layer) {
+                    if(layer.getVisible() && layerName === undefined) {
+                        layerName = layer.get('layerName');
+                    }
+                });
+                var downloadPromise = PrintService.createDownload(
+                    scope.outputFormat.value,
+                    layerName,
+                    scope.streetIndex,
+                    LayertreeService.selectedPoiTypes,
+                    LayertreeService.selectedTrackTypes
+                );
+
+                downloadPromise.then(function(url) {
+                    scope.downloadUrl = url;
+                });
             };
             // if we assign pageSize = value in template angular put only a reverence
             // into scope.pageSize and typing somethink into width/height input fields
@@ -33,7 +52,6 @@ angular.module('anol.print', [])
             };
         },
         controller: function($scope, $element, $attrs) {
-
             $scope.isPageSize = function(size) {
                 return angular.equals(size, $scope.pageSize);
             };
@@ -43,6 +61,7 @@ angular.module('anol.print', [])
             $scope.scale = angular.copy(PrintService.defaultScale);
             $scope.streetIndex = false;
             $scope.licenceAgreed = false;
+            $scope.downloadUrl = false;
 
             $scope.outputFormat = $scope.outputFormats[0];
 
