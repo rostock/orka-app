b6a9ed9b6c92e44d2e5391f4e9058b1b173ce433
Kai Culemann <culemann@omniscale.de>
2014-11-13 11:52:40 +0100

    Added and used html filter

--- app/anol/modules/anol-module.js
+++ app/anol/modules/anol-module.js
@@ -1,5 +1,11 @@
 angular.module('anol', [])
-.constant('DefaultMapName', 'anol-map');
+.constant('DefaultMapName', 'anol-map')
+// found at http://stackoverflow.com/a/21098541
+.filter('html',['$sce', function($sce) {
+    return function(input){
+        return $sce.trustAsHtml(input);
+    };
+}]);
 // TODO: We need a structure for modules
 angular.module('anol.map', []);
 angular.module('anol.scale', []);
