973d38c62da08d6f19b49f1cbd097694080331aa
Kai Culemann <culemann@omniscale.de>
2014-11-17 10:34:22 +0100

    Add coordinateFormat function to controlOptions if present

--- app/anol/modules/mouseposition/mouseposition-directive.js
+++ app/anol/modules/mouseposition/mouseposition-directive.js
@@ -6,10 +6,16 @@ angular.module('anol.mouseposition', [])
         require: '?^anolMap',
         scope: {},
         link: {
-            pre: function(scope, element, attrs, AnolMapController) {
+            pre: function(scope, element, attrs) {
                 scope.map = MapService.getMap();
-
+            },
+            post: function(scope, element, attrs, AnolMapController) {
                 var controlOptions = {};
+
+                if(angular.isFunction(scope.coordinateFormat)) {
+                    controlOptions.coordinateFormat = scope.coordinateFormat;
+                }
+
                 if(angular.isUndefined(AnolMapController)) {
                     controlOptions = {
                         target: element[0]
