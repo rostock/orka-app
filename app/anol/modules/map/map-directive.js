376cfab3423336fc7ec33449039fbd8b22634996
Kai Culemann <culemann@omniscale.de>
2014-11-17 09:17:16 +0100

    Auto resize map when browser window height changes

--- app/anol/modules/map/map-directive.js
+++ app/anol/modules/map/map-directive.js
@@ -2,11 +2,18 @@ angular.module('anol.map')
 
 .directive('anolMap', ['DefaultMapName', 'MapService', function(DefaultMapName, MapService) {
     return {
-        scope: {},
+        scope: {
+            autoResize: '@autoResize'
+        },
         link: {
             pre: function(scope, element, attrs) {
                 scope.mapName = DefaultMapName;
                 scope.map = MapService.getMap();
+
+                scope.setContainerHeight = function(height) {
+                    element.css('height', height);
+                    scope.map.updateSize();
+                };
             },
             post: function (scope, element, attrs) {
                 element
@@ -14,6 +21,22 @@ angular.module('anol.map')
                     .addClass(scope.mapName);
 
                 scope.map.setTarget(scope.mapName);
+
+                if(attrs.autoResize==='height' || attrs.autoResize === 'both') {
+                    scope.$watch(function() {
+                        return window.innerHeight;
+                    }, function(newVal, oldVal) {
+                        if(newVal !== undefined) {
+                            // TODO update when support others than px
+                            var marginTop = element.css('margin-top');
+                            marginTop = parseInt(marginTop.slice(0, marginTop.length -2));
+
+                            var marginBottom = element.css('margin-bottom');
+                            marginBottom = parseInt(marginBottom.slice(0, marginBottom.length -2));
+                            scope.setContainerHeight(newVal - marginTop - marginBottom);
+                        }
+                    });
+                }
             }
         },
         controller: function($scope, $element, $attrs) {
