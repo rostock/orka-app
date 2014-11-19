e89966a5f05bd2e1ea0718135acda2cca66e3165
Kai Culemann <culemann@omniscale.de>
2014-11-19 13:32:17 +0100

    Added orka-legend to map

--- app/orka/modules/layertree/layertree-legend-directive.js
+++ app/orka/modules/layertree/layertree-legend-directive.js
@@ -3,14 +3,23 @@ angular.module('orka.layertree')
 .directive('orkaLayertreeLegend', ['LayertreeService', function(LayertreeService) {
     return {
         restrict: 'A',
-        transclude: true,
+        require: '?^anolMap',
         templateUrl: 'orka/modules/layertree/templates/layertree-legend.html',
         scope: {},
         link: {
-            pre: function(scope, element, attrs) {
+            pre: function(scope, element, attrs, AnolMapController) {
                 scope.selectedPois = LayertreeService.selectedPoiTypes;
                 scope.selectedTracks = LayertreeService.selectedTrackTypes;
                 scope.typeMap = LayertreeService.typeMap;
+
+                 if(angular.isDefined(AnolMapController)) {
+                    element.addClass('ol-unselectable');
+                    AnolMapController.getMap().addControl(
+                        new ol.control.Control({
+                            element: element.first().context
+                        })
+                    );
+                }
             },
             post: function(scope, element, attrs) {
                 scope.$watch(function() {
