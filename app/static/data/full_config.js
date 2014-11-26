45273edbd78b662b6f46037fb9ab13253a78d6c4
Kai Culemann <culemann@omniscale.de>
2014-11-26 15:44:06 +0100

    Added full app config

---
+++ app/static/data/full_config.js
@@ -0,0 +1,20 @@
+var orkaAppConfig = {
+    map: {
+        center: [312313.88958, 5996030.31389],
+        zoom: 14,
+        layers: [
+            'ORKA_STADTPLAN',
+            'ORKA_STADTPLAN_OHNE_TEXT',
+            'ORKA_STADTPLAN_GRAU',
+            'ORKA_STADTPLAN_GRAU_OHNE_TEXT',
+            'ORKA_STADTPLAN_SCHUMMERUNG',
+            'ORKA_STADTPLAN_SCHUMMERUNG_OHNE_TEXT'
+        ]
+    },
+    themes: true,
+    print: true,
+    header: {
+        text: 'ORKaMVP - FullApp',
+        height: '50px'
+    }
+};
