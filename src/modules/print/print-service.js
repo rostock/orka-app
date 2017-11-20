8fefb9bfa25727195be85a821ad5861a77b657ad
Sebastian Gutzeit <sebastian.gutzeit@rostock.de>
2017-11-20 12:39:26 +0100

    Redmine #1339 (PDF Druck funktioniert unter IE nicht)

--- src/modules/print/print-service.js
+++ src/modules/print/print-service.js
@@ -1,4 +1,19 @@
 angular.module('orka.print')
+
+.config(['$httpProvider', function($httpProvider) {
+    
+    // initialize get if not there
+    if (!$httpProvider.defaults.headers.get) {
+        $httpProvider.defaults.headers.get = {};    
+    }    
+
+    // disable IE ajax request caching
+    $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+    // extra
+    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+}])
+
 /**
  * @ngdoc object
  * @name orka.print.PrintServiceProvider
