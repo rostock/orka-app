50465e44aaebb32ae4485dedcef8332255d96a1f
Sebastian Gutzeit <sebastian.gutzeit@rostock.de>
2020-02-17 09:56:03 +0100

    app: updated license links

--- src/modules/config/config-service.js
+++ src/modules/config/config-service.js
@@ -87,8 +87,8 @@ angular.module('orka.config')
             zoom: 2
         },
         attributions: [
-            '<div>Kartenbild © Hanse- und Universitätsstadt Rostock (<a rel="license" target="_blank" href="http://creativecommons.org/licenses/by/4.0/deed.de">CC BY 4.0</a>)<div>',
-            '<div>Kartendaten © <a target="_blank" href="http://www.openstreetmap.org">OpenStreetMap</a> (<a rel="license" target="_blank" href="http://opendatacommons.org/licenses/odbl">ODbL</a>) und LkKfS-MV</div>',
+            '<div>Kartenbild © Hanse- und Universitätsstadt Rostock (<a rel="license" target="_blank" href="https://creativecommons.org/licenses/by/4.0/deed.de">CC BY 4.0</a>)<div>',
+            '<div>Kartendaten © <a target="_blank" href="https://www.openstreetmap.org/">OpenStreetMap</a> (<a rel="license" target="_blank" href="https://opendatacommons.org/licenses/odbl/index.html">ODbL</a>) und LkKfS-MV</div>',
             '<div>Daten für Overlay-Themen © OpenStreetMap (ODbL)</div>'
         ],
         backgroundLayer: {
--- static/data/full_config.js
+++ static/data/full_config.js
@@ -16,8 +16,8 @@ var orkaAppConfig = {
         ],
         // Festlegung der Kartenattributionen
         attributions: [
-            '<div>Kartenbild © Hanse- und Universitätsstadt Rostock (<a rel="license" target="_blank" href="http://creativecommons.org/licenses/by/4.0/deed.de">CC BY 4.0</a>)<div>',
-            '<div>Kartendaten © <a target="_blank" href="http://www.openstreetmap.org">OpenStreetMap</a> (<a rel="license" target="_blank" href="http://opendatacommons.org/licenses/odbl">ODbL</a>) und LkKfS-MV</div>',
+            '<div>Kartenbild © Hanse- und Universitätsstadt Rostock (<a rel="license" target="_blank" href="https://creativecommons.org/licenses/by/4.0/deed.de">CC BY 4.0</a>)<div>',
+            '<div>Kartendaten © <a target="_blank" href="https://www.openstreetmap.org/">OpenStreetMap</a> (<a rel="license" target="_blank" href="https://opendatacommons.org/licenses/odbl/index.html">ODbL</a>) und LkKfS-MV</div>',
             '<div>Daten für Overlay-Themen © OpenStreetMap (ODbL)</div>',
             '<div>Gitternetz berechnet mit <a href="https://plus.codes">Plus codes</a></div>'
         ],
